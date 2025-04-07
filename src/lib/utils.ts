0a3d200af785f62a7256f4fd0cb053f180bc1104
psuarezdev <pablotablero@gmail.com>
2025-04-07 19:19:41 +0100

    Migrate from npm to pnpm and Add date-fns

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,23 +4,3 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function getAgeFromBirthdate(birthdate: string) {
-  const diff: number = new Date().getTime() - new Date(birthdate).getTime();
-  return Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
-}
-
-export function getDifferenceBetweenDates(startDate: string, endDate: string) {
-  const diffInMs = new Date(endDate).getTime() - new Date(startDate).getTime();
-
-  const msInDay = 1000 * 60 * 60 * 24;
-  const msInYear = msInDay * 365.25;
-
-  const diffInDays = Math.floor(diffInMs / msInDay);
-  const diffInYears = Math.floor(diffInMs / msInYear);
-  const diffInMonths = Math.floor(diffInMs / (msInDay * 30.44));
-
-  if(diffInYears > 0) return `${diffInYears} año${diffInYears > 1 ? 's' : ''}`;
-  if(diffInMonths > 0) return `${diffInMonths} mes${diffInMonths > 1 ? 'es' : ''}`;
-  return `${diffInDays} día${diffInDays > 1 ? 's' : ''}`;
-}
